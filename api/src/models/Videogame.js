c1a0b62aec2102c1e6474b7cc148b8d3e43d1ebe
Pablolsd2501~ <pablot_comercial.alamo@hotmail.com>
2023-01-31 19:53:09 -0600

    Primer commit pcon models y routes añadido

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,50 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define("videogame", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+      validate: {
+        isFloat: true,
+      },
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isUrl: true,
+      },
+    },
+    platforms: {
       type: DataTypes.STRING,
       allowNull: false,
     },
   });
 };
+/* [ ] Videojuego con las siguientes propiedades:
+ID: * No puede ser un ID de un videojuego ya existente en la API rawg
+Nombre *
+Descripción *
+Fecha de lanzamiento
+Rating
+Plataformas * */
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,19 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const videogame = require('./videogame');
+const videogames = require('./videogames');
+const genres = require('./genres');
+const createVideogame = require('./createVideogame');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use('/videogame', videogame);
+router.use('/videogames', videogames);
+router.use('/genres', genres);
+router.use('/videogame', createVideogame);
 
 
 module.exports = router;
